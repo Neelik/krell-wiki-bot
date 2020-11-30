2fd28f623f6b2bdeff6dd44ab378fb94cca57bf7
Garrett Allen <neelik.n33l@gmail.com>
2020-11-29 18:24:18 -0700

    Added simplistic dice roller.

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from index import update_index
 from pages.page import Page
 from titlecase import titlecase
+import random
 import requests
 import sys
 import os
@@ -56,6 +57,40 @@ async def check(ctx, *name):
         embed.add_field(name="Character not found :sob:", value="{} could not be found on the Wiki".format(name_cased))
         await ctx.send(embed=embed)
 
+
+@bot.command()
+@commands.is_owner()
+async def roll(ctx, dice):
+    """
+
+    :param dice: String of what dice to roll and how many, plus modifier of the form: "1d4+2"
+    :return: String representing each individual dice and their sum
+    """
+    modifier = 0
+
+    # Parse the dice
+    no_spaces = "".join(dice.split())
+
+    # check for modifier
+    if "+" in no_spaces:
+        modifier = no_spaces.split("+")[-1]
+        no_mod = no_spaces[:-2]
+    else:
+        no_mod = no_spaces
+
+    splits = no_mod.split("d")
+    num_dice = int(splits[0])
+    die = int(splits[1])
+
+    rolls = []
+    for roll in range(num_dice):
+        rolls.append(random.randint(1, die))
+
+    results = " + ".join(rolls)
+    results = f"{results} + {modifier} = {sum(rolls) + modifier}"
+    embed = Embed(title=f"Rolling {dice}")
+    embed.add_field(name="You rolled:", value=results)
+    await ctx.send(embed=embed)
 
 @bot.command()
 @commands.is_owner()
